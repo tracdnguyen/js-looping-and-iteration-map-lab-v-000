c5909e219e9526698d544cdec7b2bf027da83eb8
Trac Nguyen <nguyendtrac@gmail.com>
2018-10-28 16:13:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Code your solution in this file.
-function lowerCaseDrivers() {
-
+function lowerCaseDrivers(string, cb) {
+  return string.map(cb(something) {
+    return something === string
+  })
 }
